76452c8101192ffd31999d285c228f04d353a0e5
Marcin <kuklam@op.pl>
2020-11-19 13:14:22 +0100

    refresh by event subscription implemented

--- src/app/materials/materials.component.ts
+++ src/app/materials/materials.component.ts
@@ -3,6 +3,8 @@ import {MaterialService} from '../material.service';
 
 
 import {Material} from './material';
+import {NavigationEnd, NavigationStart, Router} from '@angular/router';
+import {NavigationEvent} from '@ng-bootstrap/ng-bootstrap/datepicker/datepicker-view-model';
 
 @Component({
   selector: 'app-materials',
@@ -19,8 +21,8 @@ export class MaterialsComponent implements OnChanges, OnInit, AfterContentChecke
   deleTedMaterialMessage: any;
   updatedMaterrial: Material;
 
-  constructor(private materialService: MaterialService) {
-    this.getMaterials();
+  constructor(private materialService: MaterialService,
+              private router: Router) {
   }
 
   getMaterials(): void {
@@ -36,18 +38,10 @@ export class MaterialsComponent implements OnChanges, OnInit, AfterContentChecke
   deleteMaterialById(id: number): any {
     this.materialService.deleteMaterialById(String(id))
       .subscribe((data) => {
-          this.deleTedMaterialMessage = data;
-          this.getMaterials();
-        }
-      );
-    /*let deletedMaterial: Material;
-    this.materials.forEach(material => {
-      if (material.id === id) {
-        deletedMaterial = material;
-      }
-    });
-    console.log(`deletedMaterial= ${deletedMaterial}`);
-    this.materials.splice(this.materials.indexOf(deletedMaterial));*/
+        this.deleTedMaterialMessage = data;
+        this.getMaterials();
+
+      });
   }
 
   updateMaterialById(id: string, material: Material): void {
@@ -59,9 +53,9 @@ export class MaterialsComponent implements OnChanges, OnInit, AfterContentChecke
         }
       );
   }
-
-
   ngOnInit(): void {
+    this.getMaterials();
+    this.refreshComponentVievAfterNavigation();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -74,6 +68,15 @@ export class MaterialsComponent implements OnChanges, OnInit, AfterContentChecke
   }
 
   ngAfterViewChecked(): void {
+
   }
+
+  refreshComponentVievAfterNavigation(): void  {
+  this.router.events.subscribe((evt) => {
+  if (evt) {
+    this.getMaterials();
+  }
+});
+}
 
 }
