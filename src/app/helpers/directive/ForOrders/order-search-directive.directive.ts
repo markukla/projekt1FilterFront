d6068b21f5b6045132525f385ba192c2b2635351
Marcin <kuklam@op.pl>
2020-12-15 00:16:14 +0100

    orderSearchDirectiveImplemented

--- src/app/helpers/directive/ForOrders/order-search-directive.directive.ts
+++ src/app/helpers/directive/ForOrders/order-search-directive.directive.ts
@@ -1,10 +1,13 @@
-import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/core';
-import {BusinesPartnerBackendService} from '../../../BusinessPartners/business-partners/BusinessPartnerServices/busines-partner-backend.service';
+import {AfterContentChecked, Directive, ElementRef, HostListener, Input, OnInit, Renderer2} from '@angular/core';
+import {OrderBackendService} from '../../../Orders/orders/OrderServices/order-backend.service';
+import {AuthenticationService} from '../../../LoginandLogOut/AuthenticationServices/authentication.service';
+import RoleEnum from '../../../Users/users/userTypes/roleEnum';
+import {OrderTableService} from '../../../Orders/orders/OrderServices/order-table.service';
 
 @Directive({
   selector: '[appOrderSearchDirective]'
 })
-export class OrderSearchDirectiveDirective {
+export class OrderSearchDirectiveDirective implements OnInit, AfterContentChecked {
 
   @Input('appOrderSearchDirective') searchedArray: Array<any>;
 
@@ -14,50 +17,68 @@ export class OrderSearchDirectiveDirective {
 
   // tslint:disable-next-line:max-line-length
   @Input() serchedColumn: string; /* insted of this input i could just declate atribute name in html(it creates completly new Atribute in html) than i can access to its value by target elements object */
- /* @Input() searchedProperty: any; */
+  /* @Input() searchedProperty: any; */
   temporatyArray: Array<any> = [];
   allowOrginalArrayCopyChange: boolean;
 
 
   constructor(private renderer: Renderer2,
               private targetElement: ElementRef,
-              private backendService: BusinesPartnerBackendService) {
+              private backendService: OrderBackendService,
+              private tableService: OrderTableService,
+              private authenticationService: AuthenticationService) {
     this.allowOrginalArrayCopyChange = true;
   }
+
   @HostListener('input')
 // tslint:disable-next-line:typedef
   serchTable() {
-    this.backendService.getAllRecords().subscribe((users) => {
-      this.orginalArrayCopy = users.body;
-      const elem = this.targetElement.nativeElement;
 
-      // tslint:disable-next-line:max-line-length
-      /*remember using directives it is better to access element property using target element than doing some extra binding with ng model etc */
-      this.serchCondition = elem.value;
-      this.temporatyArray.length = 0;
-      this.searchedArray.length = 0;
-      this.temporatyArray = this.orginalArrayCopy.filter( (x) => {
-        /* if (this.searchedProperty) {
-           x[this.searchedProperty][this.serchedColumn].includes(this.serchCondition);
-        }*/
-          x[this.serchedColumn].includes(this.serchCondition);
-      });
+    const elem = this.targetElement.nativeElement;
 
-      // tslint:disable-next-line:max-line-length
-      /*when i use filter to serch array it some how does not work, because it is creating new instance of array and directive is bind to previous instance*/
-      /*that is why a remove all elements in orginal array and push there temporary array values*/
-      this.searchedArray.length = 0;
-      this.searchedArray.push(...this.temporatyArray);
-
-
-      // tslint:disable-next-line:prefer-for-of
-      for (let i = 0; i < this.searchedArray.length; i++) {
-        console.log(this.searchedArray[i]);
-      }
-
+    // tslint:disable-next-line:max-line-length
+    /*remember using directives it is better to access element property using target element than doing some extra binding with ng model etc */
+    this.serchCondition = elem.value;
+    this.temporatyArray.length = 0;
+    this.searchedArray.length = 0;
+    this.temporatyArray = this.orginalArrayCopy.filter((x) => {
+      /* if (this.searchedProperty) {
+         x[this.searchedProperty][this.serchedColumn].includes(this.serchCondition);
+      }*/
+      x[this.serchedColumn].includes(this.serchCondition);
     });
+
+    // tslint:disable-next-line:max-line-length
+    /*when i use filter to serch array it some how does not work, because it is creating new instance of array and directive is bind to previous instance*/
+    /*that is why a remove all elements in orginal array and push there temporary array values*/
+    this.searchedArray.length = 0;
+    this.searchedArray.push(...this.temporatyArray);
+
+
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < this.searchedArray.length; i++) {
+      console.log(this.searchedArray[i]);
+    }
   }
 
+  ngOnInit(): void {
+  }
 
-
+  ngAfterContentChecked(): void {
+    this.orginalArrayCopy.length = 0;
+    if (this.authenticationService.userRole === RoleEnum.PARTNER) {
+      this.backendService.getCurrentOrdersForPartners(this.authenticationService.user.businesPartnerCompanyName).subscribe((orders) => {
+        orders.body.forEach((order) => {
+          this.orginalArrayCopy.push(this.tableService.createOrderTableCellFromOrderEntity(order));
+        });
+      });
+    }
+    else {
+      this.backendService.getCurrentOrdersForPrivilligedUsers().subscribe((orders) => {
+        orders.body.forEach((order) => {
+          this.orginalArrayCopy.push(this.tableService.createOrderTableCellFromOrderEntity(order));
+        });
+      });
+    }
+  }
 }
